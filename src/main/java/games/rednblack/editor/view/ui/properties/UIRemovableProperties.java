613a79eba96a771eeea64eca97ed1844f4456b14
MiChinao <fgnm@gmail.com>
2020-08-01 19:17:07 +0200

    Do not collapse properties on close button click

--- src/main/java/games/rednblack/editor/view/ui/properties/UIRemovableProperties.java
+++ src/main/java/games/rednblack/editor/view/ui/properties/UIRemovableProperties.java
@@ -1,7 +1,9 @@
 package games.rednblack.editor.view.ui.properties;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.util.dialog.Dialogs;
@@ -36,10 +38,9 @@ public abstract class UIRemovableProperties extends UIItemCollapsibleProperties 
                 header.setBackground(VisUI.getSkin().getDrawable("expandable-properties-" + (collapsibleWidget.isCollapsed() ? "inactive" : "active") + "-bg"));
             }
         });
-        closeButton.addListener(new ClickListener() {
+        closeButton.addListener(new ChangeListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
+            public void changed (ChangeEvent event, Actor actor) {
                 Dialogs.showConfirmDialog(Sandbox.getInstance().getUIStage(),
                         "Delete Component", "Do you want to delete this component?",
                         new String[]{"Cancel", "Delete"}, new Integer[]{0, 1}, r -> {
@@ -48,6 +49,13 @@ public abstract class UIRemovableProperties extends UIItemCollapsibleProperties 
                                 remove();
                             }
                         }).padBottom(20).pack();
+            }
+        });
+        closeButton.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                event.cancel();
+                return true;
             }
         });
         return header;
