3af4f7a43764601cdde1f7f2bcb228e9e31dfc62
fgnm <fgnm.dev@gmail.com>
2020-11-29 18:04:03 +0100

    Fix Sticky Note position with PPWU

--- src/main/java/games/rednblack/editor/controller/commands/CreateStickyNoteCommand.java
+++ src/main/java/games/rednblack/editor/controller/commands/CreateStickyNoteCommand.java
@@ -20,8 +20,8 @@ public class CreateStickyNoteCommand extends RevertibleCommand {
         SceneVO sceneVO = sandbox.getSceneControl().getCurrentSceneVO();
         StickyNoteVO noteVO = new StickyNoteVO();
         noteVO.id = id;
-        noteVO.x = position.x - 3;
-        noteVO.y = position.y - 30;
+        noteVO.x = position.x - 9f / sandbox.getPixelPerWU();
+        noteVO.y = position.y - (noteVO.height - 9f) / sandbox.getPixelPerWU();
 
         sceneVO.composite.sStickyNotes.put(id, noteVO);
 
