493f551bb5b92234a75d7a26593b953f50677813
fgnm <fgnm.dev@gmail.com>
2021-01-10 18:58:49 +0100

    Keep things simple

--- src/main/java/games/rednblack/editor/view/ui/followers/SpineFollower.java
+++ src/main/java/games/rednblack/editor/view/ui/followers/SpineFollower.java
@@ -4,7 +4,6 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Affine2;
 import com.badlogic.gdx.math.Matrix4;
 import com.esotericsoftware.spine.SkeletonRendererDebug;
 import games.rednblack.editor.renderer.utils.ComponentRetriever;
@@ -16,7 +15,6 @@ public class SpineFollower extends NormalSelectionFollower {
     private final SkeletonRendererDebug skeletonRendererDebug;
 
     private final Matrix4 matrix = new Matrix4();
-    private final Affine2 affine2 = new Affine2();
 
     public SpineFollower(Entity entity) {
         super(entity);
@@ -33,22 +31,9 @@ public class SpineFollower extends NormalSelectionFollower {
         if (!Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
             batch.end();
 
+            matrix.set(transformComponent.worldTransform);
+
             skeletonRendererDebug.getShapeRenderer().setProjectionMatrix(Sandbox.getInstance().getCamera().combined);
-
-            float originX = transformComponent.originX;
-            float originY = transformComponent.originY;
-            float x = transformComponent.x;
-            float y = transformComponent.y;
-            float rotation = transformComponent.rotation;
-            float scaleX = transformComponent.scaleX;
-            float scaleY = transformComponent.scaleY;
-
-            affine2.setToTrnRotScl(x + originX , y + originY, rotation, scaleX, scaleY);
-            if (originX != 0 || originY != 0) affine2.translate(-originX, -originY);
-            affine2.translate(-spineObjectComponent.minX, -spineObjectComponent.minY);
-
-            matrix.set(affine2);
-
             skeletonRendererDebug.getShapeRenderer().setTransformMatrix(matrix);
 
             skeletonRendererDebug.draw(spineObjectComponent.skeleton);
