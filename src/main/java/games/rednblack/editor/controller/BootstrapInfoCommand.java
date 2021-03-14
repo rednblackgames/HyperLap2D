b2ca87e64417b901d7dcb3f8f9a644cf38bce2d7
MiChinao <fgnm@gmail.com>
2021-03-14 16:12:21 +0100

    The most weird bug I've ever found pt.2 clearColor inside FBO freeze rendering on some Intel HD Graphics GPU unless custom Fragment Shader is provided see @DefaultShaders#DEFAULT_FRAGMENT_SHADER

--- src/main/java/games/rednblack/editor/controller/BootstrapInfoCommand.java
+++ src/main/java/games/rednblack/editor/controller/BootstrapInfoCommand.java
@@ -1,7 +1,9 @@
 package games.rednblack.editor.controller;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import games.rednblack.editor.renderer.systems.render.HyperLap2dRenderer;
 import games.rednblack.editor.utils.AppConfig;
 import games.rednblack.h2d.common.HyperLog;
 import org.apache.commons.lang3.SystemUtils;
@@ -15,7 +17,16 @@ public class BootstrapInfoCommand extends SimpleCommand {
         HyperLog.infoUnderline("HyperLap2D " + AppConfig.getInstance().versionString);
         HyperLog.info(SystemUtils.OS_NAME + " [" + SystemUtils.OS_VERSION + " - " + SystemUtils.OS_ARCH + "]");
         GL20 gl20 = Gdx.graphics.getGL20();
-        HyperLog.info(gl20.glGetString(GL20.GL_VENDOR) + " " + gl20.glGetString(GL20.GL_RENDERER));
+        String gpu = gl20.glGetString(GL20.GL_RENDERER);
+        if (gpu.toLowerCase().contains("hd graphics")) {
+            /*
+            TODO the most weird bug I've ever found!
+            clearColor inside FBO freeze rendering on some Intel HD Graphics GPU
+            unless custom Fragment Shader is provided see @DefaultShaders#DEFAULT_FRAGMENT_SHADER
+             */
+            HyperLap2dRenderer.clearColor = new Color(0.000001f, 0, 0, 0);
+        }
+        HyperLog.info(gl20.glGetString(GL20.GL_VENDOR) + " - " + gpu);
         HyperLog.info("Screen size " + Gdx.graphics.getBackBufferWidth() + " x " + Gdx.graphics.getBackBufferHeight());
         HyperLog.info("GL version " + gl20.glGetString(GL20.GL_VERSION));
         HyperLog.info("Shaders version " + gl20.glGetString(GL20.GL_SHADING_LANGUAGE_VERSION));
