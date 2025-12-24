5e5884f266bc620b0876f82f716976ae1b87346f
fgnm <fgnm.dev@gmail.com>
2025-12-24 11:37:03 +0100

    match latest upstream

--- src/main/java/com/badlogic/gdx/backends/lwjgl3/Lwjgl3ApplicationGLESFix.java
+++ src/main/java/com/badlogic/gdx/backends/lwjgl3/Lwjgl3ApplicationGLESFix.java
@@ -26,8 +26,8 @@ import com.badlogic.gdx.backends.lwjgl3.audio.Lwjgl3Audio;
 import com.badlogic.gdx.backends.lwjgl3.audio.OpenALLwjgl3Audio;
 import com.badlogic.gdx.graphics.glutils.GLVersion;
 
+import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.utils.*;
-import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.AMDDebugOutput;
@@ -475,7 +475,9 @@ public class Lwjgl3ApplicationGLESFix implements Lwjgl3ApplicationBase {
         GLFW.glfwWindowHint(GLFW.GLFW_DEPTH_BITS, config.depth);
         GLFW.glfwWindowHint(GLFW.GLFW_SAMPLES, config.samples);
 
-        if (config.glEmulation == Lwjgl3ApplicationConfiguration.GLEmulation.GL30) {
+        if (config.glEmulation == Lwjgl3ApplicationConfiguration.GLEmulation.GL30
+                || config.glEmulation == Lwjgl3ApplicationConfiguration.GLEmulation.GL31
+                || config.glEmulation == Lwjgl3ApplicationConfiguration.GLEmulation.GL32) {
             GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, config.gles30ContextMajorVersion);
             GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, config.gles30ContextMinorVersion);
             if (SharedLibraryLoader.os == Os.MacOsX) {
@@ -544,14 +546,9 @@ public class Lwjgl3ApplicationGLESFix implements Lwjgl3ApplicationBase {
                         monitorHandle = config.maximizedMonitor.monitorHandle;
                     }
 
-                    IntBuffer areaXPos = BufferUtils.createIntBuffer(1);
-                    IntBuffer areaYPos = BufferUtils.createIntBuffer(1);
-                    IntBuffer areaWidth = BufferUtils.createIntBuffer(1);
-                    IntBuffer areaHeight = BufferUtils.createIntBuffer(1);
-                    GLFW.glfwGetMonitorWorkarea(monitorHandle, areaXPos, areaYPos, areaWidth, areaHeight);
-
-                    GLFW.glfwSetWindowPos(windowHandle, Math.max(0, areaXPos.get(0) + areaWidth.get(0) / 2 - windowWidth / 2),
-                            Math.max(0, areaYPos.get(0) + areaHeight.get(0) / 2 - windowHeight / 2));
+                    GridPoint2 newPos = Lwjgl3ApplicationConfiguration.calculateCenteredWindowPosition(
+                            Lwjgl3ApplicationConfiguration.toLwjgl3Monitor(monitorHandle), windowWidth, windowHeight);
+                    GLFW.glfwSetWindowPos(windowHandle, newPos.x, newPos.y);
                 } else {
                     GLFW.glfwSetWindowPos(windowHandle, config.windowX, config.windowY);
                 }
