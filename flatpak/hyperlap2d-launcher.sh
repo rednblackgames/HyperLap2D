83564997f6aa69757b788c1d191b20e1eef29f0c
fgnm <fgnm.dev@gmail.com>
2025-12-28 20:53:08 +0100

    Revert "try to include jre in flatpak"
    This reverts commit 3b65b1ea10d65360eb9af730ef444221e3d3f375.

--- flatpak/hyperlap2d-launcher.sh
+++ flatpak/hyperlap2d-launcher.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-export JAVA_HOME=/app/jre
-export PATH=$JAVA_HOME/bin:$PATH
-
-exec /app/jre/bin/java \
+exec /usr/lib/sdk/openjdk21/bin/java \
     -XX:+UseZGC -Xms128m \
     -Djava.library.path=/app/share/hyperlap2d/lib \
     -cp "/app/share/hyperlap2d/lib/*" \
