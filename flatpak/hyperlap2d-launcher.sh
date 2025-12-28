3b65b1ea10d65360eb9af730ef444221e3d3f375
fgnm <fgnm.dev@gmail.com>
2025-12-28 20:40:25 +0100

    try to include jre in flatpak

--- flatpak/hyperlap2d-launcher.sh
+++ flatpak/hyperlap2d-launcher.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-exec /usr/lib/sdk/openjdk21/bin/java \
+export JAVA_HOME=/app/jre
+export PATH=$JAVA_HOME/bin:$PATH
+
+exec /app/jre/bin/java \
     -XX:+UseZGC -Xms128m \
     -Djava.library.path=/app/share/hyperlap2d/lib \
     -cp "/app/share/hyperlap2d/lib/*" \
